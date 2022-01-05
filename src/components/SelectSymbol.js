fbf483784184ae802968ea2b3813b955e0682631
vaibhavbuccha <vincent@ninjatechnolabs.com>
2022-01-05 16:17:18 +0530

    'Fix-set-symbol'

--- src/components/SelectSymbol.js
+++ src/components/SelectSymbol.js
@@ -8,10 +8,10 @@ export default function SelectSymbol({ symbol, setSymbol }) {
         <div className="text-center pt-2" >
             <h5>Select Symbol.</h5>
             <div className="d-flex justify-content-center p-3"  >
-                <div className="col-sm-1" > 
+                <div className="col-sm-2 m-1" > 
                     <button className={ symbol === 'X' ? "btn btn-success" : "btn btn-warning" } onClick={() => handleSymbolSelection('X')} >X</button>
                 </div>
-                <div className="col-sm-1" >
+                <div className="col-sm-2 m-1" >
                     <button className={ symbol === 'O' ? "btn btn-success" : "btn btn-warning" } onClick={() => handleSymbolSelection('O')} >O</button>
                 </div>
             </div>
